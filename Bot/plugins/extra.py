b8c4d48b3d90af022629eb2747e6241b34733cf2
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2023-11-11 15:28:18 +0530

    Update extra.py
    Signed-off-by: Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>

--- Bot/plugins/extra.py
+++ Bot/plugins/extra.py
@@ -5,5 +5,12 @@ from Bot import TRIGGERS as trg, OWNER_ID
 
 @Client.on_message(filters.user(OWNER_ID) & filters.command('restart', prefixes=trg))
 async def restart_bot(client: Client, message: Message):  
-    msg = await message.reply("Restarting", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Dev', url='https://t.me/sohailkhan_indianime')]]))
-    os.execl(sys.executable, sys.executable, "-m", "Bot")  
+    msg = await message.reply("Restarting...")
+    time.sleep(5) # Adding a 5-second delay before restarting
+    os.execl(sys.executable, sys.executable, "-m", "Bot")
+
+@Client.on_ready()
+async def bot_restarted(client: Client):
+    me = await client.get_me()
+    restart_notif = f"Bot is successfully restarted! @{me.username}"
+    msg = await message.reply(restart_notif, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Dev', url='https://t.me/BashAFK')]]))
